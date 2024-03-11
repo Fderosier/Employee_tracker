abaa8b40d1fb1184e98cf322e3e2843028c74d58
frankiederosier <frankiederosier@Frankies-MacBook-Pro.local>
2024-03-10 21:57:28 -0400

    fixes

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,26 @@
-DROP DATABASE IF EXISTS employee_tracker_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-CREATE DATABASE employee_tracker_db;
+use employee_db;
 
-USE employee_tracker_db;
-
-CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
+    salary Decimal(10,2),
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
+
 );
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+CREATE TABLE employee(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
